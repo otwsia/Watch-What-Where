6cf5dd30dae52bc2987dd991d08be14079ae9fc1
onthewaysia <atsqw001@gmail.com>
2022-10-04 21:56:57 +0800

    fixed carousel bug

--- watch-what-where/src/components/list/list-item/ListItem.js
+++ watch-what-where/src/components/list/list-item/ListItem.js
@@ -28,22 +28,23 @@ const ListItem = (props) => {
   //   }, [hovered]);
 
   return (
-    <div
-      className={`${styles.item}`}
-      style={{ left: hovered && props.index * 225 - 50 + props.index * 2.5 }}
-      onMouseEnter={() =>
-        setHovered(
-          `https://api.themoviedb.org/3/${props.category}/${props.movie.id}/videos?api_key=decb776a782c2ad315d5469877220f3d`
-        )
-      }
-      onMouseLeave={() => setHovered("")}
-    >
-      <img
-        src={`https://image.tmdb.org/t/p/original${props.movie.backdrop_path}`}
-        alt="movie banner"
-        className={`${styles.banner}`}
-      />
-      {/* {hovered &&
+    <div className={styles.itemContainer}>
+      <div
+        className={`${styles.item}`}
+        style={{ left: hovered && props.index * 225 - 50 + props.index * 2.5 }}
+        onMouseEnter={() =>
+          setHovered(
+            `https://api.themoviedb.org/3/${props.category}/${props.movie.id}/videos?api_key=decb776a782c2ad315d5469877220f3d`
+          )
+        }
+        onMouseLeave={() => setHovered("")}
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/original${props.movie.backdrop_path}`}
+          alt="movie banner"
+          className={`${styles.banner}`}
+        />
+        {/* {hovered &&
         (trailer.site === "YouTube" ? (
           <video
             className={styles.video}
@@ -61,35 +62,36 @@ const ListItem = (props) => {
         ))}
       {console.log(hovered)}
       {console.log(trailer)} */}
-      <div className={`${styles.itemInfo}`}>
-        <div className={`${styles.icons}`}>
-          <PlayArrow className={`${styles.icon}`} />
-          <Add className={`${styles.icon}`} />
-          <span className={`${styles.stats} ${styles.border}`}>
-            {`${Math.round(props.movie.vote_average * 10)}%`}
-          </span>
-          <span className={`${styles.stats}`}>
-            {year(props.movie.release_date)}
-          </span>
-        </div>
-        <div>
-          {props.movie?.title ||
-            props.movie?.name ||
-            props.movie?.original_name}
-        </div>
-        <div className={`${styles.description}`}>
-          {truncate(props.movie.overview, 130)}
-        </div>
-        <div>
-          {props.movie.genre_ids.map((id, i) => {
-            return (
-              genre[id] && (
-                <span className={`${styles.genre}`} key={i}>
-                  {genre[id]}
-                </span>
-              )
-            );
-          })}
+        <div className={`${styles.itemInfo}`}>
+          <div className={`${styles.icons}`}>
+            <PlayArrow className={`${styles.icon}`} />
+            <Add className={`${styles.icon}`} />
+            <span className={`${styles.stats} ${styles.border}`}>
+              {`${Math.round(props.movie.vote_average * 10)}%`}
+            </span>
+            <span className={`${styles.stats}`}>
+              {year(props.movie.release_date)}
+            </span>
+          </div>
+          <div>
+            {props.movie?.title ||
+              props.movie?.name ||
+              props.movie?.original_name}
+          </div>
+          <div className={`${styles.description}`}>
+            {truncate(props.movie.overview, 130)}
+          </div>
+          <div>
+            {props.movie.genre_ids.map((id, i) => {
+              return (
+                genre[id] && (
+                  <span className={`${styles.genre}`} key={i}>
+                    {genre[id]}
+                  </span>
+                )
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
